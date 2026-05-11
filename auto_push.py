5ec2991887384bec326103f7ec1e5bb7752057d3
sumit989bishnoi-crypto <sumit989bishnoi@gmail.com>
2026-05-11 20:00:16 +0000

    Auto Commit 2026-05-11 20:00:16

--- auto_push.py
+++ auto_push.py
@@ -2,17 +2,12 @@ import os
 import time
 from datetime import datetime
 
-# =========================
-# SETTINGS
-# =========================
+
 
 REPO_URL = "https://github.com/sumit989bishnoi-crypto/auto-ai-.git"
 BRANCH = "main"
-COMMIT_INTERVAL = 6 # 10 minutes
+COMMIT_INTERVAL = 6 
 
-# =========================
-# FIRST TIME SETUP
-# =========================
 
 if not os.path.exists(".git"):
     print("[*] Initializing Git Repository...")
@@ -31,23 +26,20 @@ if not os.path.exists(".git"):
 
     os.system(f"git push -u origin {BRANCH}")
 
-# =========================
-# AUTO COMMIT LOOP
-# =========================
 
 print("[+] Auto Push Started...")
 
 while True:
     try:
-        # Add all files
+       
         os.system("git add .")
 
-        # Commit with time
+       
         commit_message = f'Auto Commit {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
         os.system(f'git commit -m "{commit_message}"')
 
-        # Push changes
+        
         os.system(f"git push origin {BRANCH}")
 
         print(f"[+] Pushed at {datetime.now()}")
